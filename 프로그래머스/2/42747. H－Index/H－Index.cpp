f970e9e26070ae6fe5fc2c115bab050c7f3a47fa
Woojin Kwak <65993764+gugyeoj1n@users.noreply.github.com>
2026-08-12 15:23:54 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+    int h = 0;
+    for(int i = 0; i < citations.size(); i++)
+    {
+        if(citations[i] >= i + 1) h = i + 1;
+        else break;
+    }
+    return h;
+}
