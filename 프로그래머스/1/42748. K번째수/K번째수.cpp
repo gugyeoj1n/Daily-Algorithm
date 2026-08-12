254dd7dadc7d36a1f803c378b81782d09cb00d24
Woojin Kwak <65993764+gugyeoj1n@users.noreply.github.com>
2026-08-12 15:06:44 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -4,17 +4,13 @@
 
 using namespace std;
 
-int getVector(vector<int> array, vector<int> command);
-
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer;
-    for(const auto& cmd: commands)
-        answer.push_back(getVector(array, cmd));
-    return answer;
-}
-
-int getVector(vector<int> array, vector<int> command) {
-    vector<int> slice(array.begin() + command[0] - 1, array.begin() + command[1]);
-    sort(slice.begin(), slice.end());
-    return slice[command[2] - 1];
+    vector<int> result;
+    for(auto x : commands)
+    {
+        vector<int> sliced(array.begin() + x[0] - 1, array.begin() + x[1]);
+        sort(sliced.begin(), sliced.end());
+        result.push_back(sliced[x[2] - 1]);
+    }
+    return result;
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.02 ms
+메모리: 4.63 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 11:24:27
+2026년 08월 12일 15:06:41
 
 ### 문제 설명
 
